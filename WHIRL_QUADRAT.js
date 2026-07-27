0f1ccace99bb5bcb8808f02126aebcbfffb9659b
iki1uc <entertrainer@web.de>
2026-07-27 10:15:53 +0200

    Add WHIRL_QUADRAT module with live function

---
+++ WHIRL_QUADRAT.js
@@ -0,0 +1,13 @@
+export const WHIRL_QUADRAT = {
+
+    live(pool) {
+        return {
+            W1: pool.WHIRL,
+            W2: pool.RESPO,
+            W3: pool.GEO,
+            W4: pool.MODI,
+            form: "WHIRL-QUADRAT",
+            orbit: "WHIRL-360°"
+        };
+    }
+};
