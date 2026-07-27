2d9050096e0bd1555065cd330a27df384ead7d0f
iki1uc <entertrainer@web.de>
2026-07-27 10:16:09 +0200

    Create WHIRL_VEC.js

---
+++ WHIRL_VEC.js
@@ -0,0 +1,15 @@
+export const WHIRL_VEC = {
+
+    compute(quadrat) {
+        return {
+            VEC: [
+                quadrat.W1,
+                quadrat.W2,
+                quadrat.W3,
+                quadrat.W4
+            ],
+            orbit: "WHIRL-360°",
+            axiom: 4
+        };
+    }
+};
