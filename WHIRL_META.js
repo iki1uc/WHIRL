f4e77cab2bdc87524abff3e2ae33bd77c2d3d037
iki1uc <entertrainer@web.de>
2026-08-17 03:49:12 +0200

    Update WHIRL_META.js

--- WHIRL_META.js
+++ WHIRL_META.js
@@ -3,9 +3,18 @@ export const WHIRL_META = {
     drift(pool) {
         return {
             mode: "META-DRIFT",
+
+            // ALT bleibt
             geo: pool.GEO,
             modi: pool.MODI,
-            effect: "DRIFT-CONTROL"
+            effect: "DRIFT-CONTROL",
+
+            // NEU
+            causeCascade: pool.causeCascade,
+            nextCause: pool.causeCascade[ pool.causeCascade.indexOf(pool.causeLevel)+1 ] || pool.causeLevel,
+            wind: pool.windLevel,
+            zooog: pool.zooogBoost,
+            whrlcausewind: pool.whrlcausewind
         };
     }
 };
