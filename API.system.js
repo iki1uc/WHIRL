73fc3a39b99f2addd090ff295d95799e3cd11d7a
iki1uc <entertrainer@web.de>
2026-08-17 03:55:21 +0200

    Update API.system.js

--- API.system.js
+++ API.system.js
@@ -1,6 +1,10 @@
-// API.system.js
 window.API_SYSTEM = {
     idMode: () => ID_UNIT.mode,
     idLevel: () => ID_UNIT.level,
-    idVector: () => ID_UNIT.vector
+    idVector: () => ID_UNIT.vector,
+
+    // NEU
+    cause: () => WHIRL_POOL.causeLevel,
+    wind: () => WHIRL_POOL.windLevel,
+    whrlcausewind: () => WHIRL_POOL.whrlcausewind
 };
