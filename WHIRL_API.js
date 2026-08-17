60eb939802e7235e874cf6e6e6a688d60c3ce454
iki1uc <entertrainer@web.de>
2026-08-17 03:54:37 +0200

    Update WHIRL_API.js

--- WHIRL_API.js
+++ WHIRL_API.js
@@ -2,10 +2,16 @@ export const WHIRL_API = {
 
     fuse(pool) {
         return {
+            // ALT
             system: pool.API,
             id: pool.ID,
             mode: "API-FUSION",
-            status: "ACTIVE"
+            status: "ACTIVE",
+
+            // NEU
+            cause: pool.causeLevel,
+            wind: pool.windLevel,
+            whrlcausewind: pool.whrlcausewind
         };
     }
 };
