a943110d068c2a783423e1062fbcdbd36c001ebf
iki1uc <entertrainer@web.de>
2026-08-17 03:54:09 +0200

    Update WHIRL_QUADRAT.js

--- WHIRL_QUADRAT.js
+++ WHIRL_QUADRAT.js
@@ -2,12 +2,19 @@ export const WHIRL_QUADRAT = {
 
     live(pool) {
         return {
+            // ALT
             W1: pool.WHIRL,
             W2: pool.RESPO,
             W3: pool.GEO,
             W4: pool.MODI,
+
             form: "WHIRL-QUADRAT",
-            orbit: "WHIRL-360°"
+            orbit: "WHIRL-360°",
+
+            // NEU
+            cause: pool.causeLevel,
+            wind: pool.windLevel,
+            whrlcausewind: pool.whrlcausewind
         };
     }
 };
